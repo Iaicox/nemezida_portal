96947c1ac1ee32d9ccaf187efbd7a5096329e6c0
Rusdiv <64499427+Rusdiv@users.noreply.github.com>
2021-03-30 21:03:23 +0300

    add header layout

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="header__name">
+      <img src="../assets/img/header/logo.svg" class="header__logo" />
+      <h1 class="header__title">гиперкорпортал</h1>
+    </div>
+    <div class="header__searchBlock">
+      <input type="text" class="header__search-input" />
+      <button class="header__search-button"></button>
+    </div>
+    <button class="header__menu"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="app-main-layout">
     <main class="app-content">
+      <Header />
       <div class="app-page">
         <router-view />
       </div>
     </main>
   </div>
 </template>
+
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+  name: "MainLayout",
+  components: { Header },
+};
+</script>
